5fb31e0f7eb8d5f4da7e696ea9b8745b96ba6cd1
agent355 <70315828+agent255@users.noreply.github.com>
2020-10-29 12:39:33 +0530

    eh

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,1 +1,10 @@
-Swal.fire("Warning!", "This site is in beta, so you may see some funky incomplete styling", "warning");
+const alerted = localStorage.getItem("alerted");
+if (alerted == "yes") {
+  console.log("Already Alerted");
+} else {
+  Swal.fire(
+    "Warning!",
+    "This site is in beta, so you may see some funky incomplete styling",
+    "warning"
+  );
+}
